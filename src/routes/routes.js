cdc7e9fca824364a6ab8883a488f0678010eb837
Wilson Collantes Aponte <wilson.aponte000@gmail.com>
2023-11-16 08:54:12 -0500

    V5 - Password decryption

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connection = require("../conexion/bd_conexion.jsx");
 const router = express.Router();
+const bcrypt = require("bcrypt");
 
 router.post("/login", async (req, res) => {
   const { user, clave } = req.body;
@@ -8,8 +9,9 @@ router.post("/login", async (req, res) => {
   try {
     const results = await new Promise((resolve, reject) => {
       connection.query(
-        "SELECT * FROM users WHERE user = ? AND clave = ?",
-        [user, clave],
+        "SELECT * FROM users WHERE user = ?",
+        // "SELECT * FROM users WHERE user = ? AND clave = ?",
+        [user],
         function (err, results) {
           if (err) {
             reject(err);
@@ -19,8 +21,21 @@ router.post("/login", async (req, res) => {
         }
       );
     });
-    console.log(results);
-    res.status(200).json(results);
+
+    if (results.length === 0) return res.status(200).json({ userExist: false });
+
+    const passwordIsCorrect = await new Promise((resolve, reject) => {
+      bcrypt.compare(clave, results[0].clave, (err, result) => {
+        if (err) reject(err);
+        if (result) resolve(result);
+        else resolve(result);
+      });
+    });
+
+    //Responde con informacón que determina si existe un usuario y clave
+    res
+      .status(200)
+      .json({ userExist: results.length !== 0, passwordIsCorrect });
   } catch (error) {
     console.error("Error en la consulta a la base de datos:", error);
     res.status(500).json({
